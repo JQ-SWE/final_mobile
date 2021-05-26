4b42a73ce044336f3e66daab3ea0582048b5bc4c
avocado-CC <1447870798@qq.com>
2021-05-26 18:30:48 +0800

    test merge

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar hToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hToolBar = findViewById(R.id.HomeToolBar);
+        setSupportActionBar(hToolBar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.additional_menu, menu);
+        return true;
+    }
+
 }
