c6995391050d6cef479a7c3ebab22fe87f1cf68b
JINQI354 <swe1809354@xmu.edu.my>
2021-07-05 16:26:57 +0800

    ui ok

--- app/src/main/java/com/example/afinal/BikeShareRentAcitvity.java
+++ app/src/main/java/com/example/afinal/BikeShareRentAcitvity.java
@@ -3,12 +3,18 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BikeShareRentAcitvity extends AppCompatActivity {
+
+    Button btScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike_share_rent_acitvity);
+
+        btScan = (Button) findViewById(R.id.btn_scan);
+
     }
 }
